0f03a9e59919ea802c6d1f41bc5b623e6bedcb07
Shivanshu Goyal <shivanshugoyal0111@gmail.com>
2016-09-21 00:09:55 +0530

    Create Codes.java

---
+++ src/main/java/com/dms/constant/Codes.java
@@ -0,0 +1,12 @@
+package com.dms.constant;
+
+/**
+ * Created by shivanshu.goyal on 15-09-2016.
+ */
+public interface Codes {
+
+    String UPLOAD_DOCUMENT_SUCCESSFUL= "upload.document.successful";
+    String DOWNLOAD_DOCUMENT_SUCCESSFUL= "download.document.successful";
+    String DELETE_DOCUMENT_SUCCESSFUL= "delete.document.successful";
+
+}
